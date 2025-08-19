7f664cb538d6f950d9abfbcb5dc50692cc1b8f86
Muhammad Fouad Ilyas Siddiqui <mfouad_ilyas@hotmail.com>
2025-08-19 18:50:45 +0500

    PythonBasics

---
+++ Chapter 1/01_variables.py
@@ -0,0 +1,39 @@
+print("Hello, Python!")
+
+"""
+print() is a built-in function that shows output on the screen.
+Whatever is inside " " (quotes) is called a string.
+"""
+
+print("========================")
+
+name = "Fouad"
+age = 25
+height = 5.9
+is_student = True
+
+print(name, age, height, is_student)
+
+"""
+Explanation:
+name = "Fouad" → Here, "Fouad" is a string stored in the variable name.
+age = 25 → 25 is an integer stored in age.
+height = 5.9 → 5.9 is a float stored in height.
+is_student = True → True is a boolean stored in is_student.
+print(name, age, height, is_student) → Displays all values.
+"""
+
+print("========================")
+
+_cars = 23
+cars = 24
+CARS = 25
+CARS = 3629383
+number_of_cars = 23
+kind_of_cars = "Ferrari"
+
+print(cars)
+print(_cars)
+print(CARS)
+print(kind_of_cars)
+
